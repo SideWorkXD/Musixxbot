a9ab875815c076d88c9b8a15f200e0493a5ac151
SideWorkXD <164920141+SideWorkXD@users.noreply.github.com>
2024-05-24 16:44:45 +0500

    Update support.js

--- basicCommands/support.js
+++ basicCommands/support.js
@@ -1,28 +1,32 @@
-const { EmbedBuilder } = require('discord.js');
+const { EmbedBuilder } = require("discord.js");
 const db = require("../mongodb");
 module.exports = {
-    name: 'support',
-    description: 'support server of this Bot',
-    execute(message, args) {
-        const supportServerLink = "https://discord.gg/xQF9f9yUEM";
-      const githubLink = "https://github.com/GlaceYT";
-      const replitLink = "https://replit.com/@GlaceYT";
-      const youtubeLink = "https://www.youtube.com/@GlaceYT";
-        const embed = new EmbedBuilder()
-            .setColor('#b300ff')
-            .setAuthor({
-              name: 'Support Server',
-              iconURL: 'https://cdn.discordapp.com/attachments/1230824451990622299/1230824519220985896/6280-2.gif?ex=6638ae28&is=66375ca8&hm=13e4a1b91a95b2934a39de1876e66c11711c7b30ac1a91c2a158f2f2ed1c2fc6&', 
-              url: 'https://discord.gg/xQF9f9yUEM'
-          })
-            .setDescription(`➡️ **Join our Discord server for support and updates:**\n- Discord - ${supportServerLink}\n\n➡️ **Follow us on:**\n- GitHub - ${githubLink}\n- Replit - ${replitLink}\n- YouTube - ${youtubeLink}`)
-            .setImage('https://cdn.discordapp.com/attachments/1113800537402527903/1236803979996958740/11.png?ex=663956f7&is=66380577&hm=3b3c19a11adcb979517a133f2907f671305d23f1f5092cf7df043e6d5cab07bc&')
-            .setTimestamp();
+  name: "support",
+  description: "support server of this Bot",
+  execute(message, args) {
+    const supportServerLink = "https://discord.gg/BsdfHF6r2M";
+    const instaLink = "https://www.instagram.com/ig.billo";
+    const twitterLink = "https://twitter.com/BilloXd";
+    const youtubeLink = "https://www.youtube.com/@iambilloxd";
+    const embed = new EmbedBuilder()
+      .setColor("#b300ff")
+      .setAuthor({
+        name: "Support Server",
+        iconURL:
+          "https://cdn.discordapp.com/attachments/1230824451990622299/1230824519220985896/6280-2.gif?ex=6638ae28&is=66375ca8&hm=13e4a1b91a95b2934a39de1876e66c11711c7b30ac1a91c2a158f2f2ed1c2fc6&",
+        url: "https://discord.gg/xQF9f9yUEM",
+      })
+      .setDescription(
+        `<:discordverify:861292720324476968> **Join our Discord server for support and updates:**\n- Discord - ${supportServerLink}\n\n <a:diamond:859462733233782815> **Follow us on:**\n- Instagram - ${instaLink}\n- Twitter - ${twitterLink}\n- YouTube - ${youtubeLink}`,
+      )
+      .setImage(
+        "https://media.discordapp.net/attachments/1230408029191540807/1232262395867693156/standard_1.gif?ex=6651afc9&is=66505e49&hm=1bc7d9abf84252e15004d2b6a677bc05b7bcfb59fc24b2ea1c993a704cc801d7&=&width=550&height=194",
+      )
+      .setTimestamp();
 
-        message.reply({ embeds: [embed] });
-    },
+    message.reply({ embeds: [embed] });
+  },
 };
-
 
 /*
 
